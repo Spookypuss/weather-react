915b07410ee4fc6bde1d3af7d142cdf88d20008f
Spookypuss <114418331+Spookypuss@users.noreply.github.com>
2024-02-06 13:59:05 +0000

    Removed unused dependency

--- src/Weather.js
+++ src/Weather.js
@@ -5,7 +5,6 @@ export default function Weather(props) {
         alert(`The current temperature in ${response.data.name} is ${Math.round(response.data.main.temp)}°C`)
     }
     
-    let city = "Glasgow";
     let apiKey = "28966f9a5b2543fb60e8a809ec2c1fd9";
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
 
